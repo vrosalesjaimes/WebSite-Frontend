873e99cc07c5139bc4bef691a8aa6c6b52e2cb68
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2024-10-23 14:39:44 -0600

    Creación de formulario de contacto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { NgxSpinnerComponent } from 'ngx-spinner';
+import { HomeComponent } from "./components/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, NgxSpinnerComponent],
+  imports: [RouterOutlet, NavbarComponent, NgxSpinnerComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,10 @@ import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { CertificationsComponent } from './components/certifications/certifications.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: '', component: HomeComponent },
     {path: 'something-missing', component: PageNotFoundComponent },  
     {path: 'projects', component: ProjectsComponent },
     {path: 'certifications', component: CertificationsComponent},
