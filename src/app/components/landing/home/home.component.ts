fe44ef7a8c9e81def10f0cdc483e96194a4aafaf
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2024-10-27 18:04:58 -0600

    allowedCommonJsDependencies modified

--- src/app/components/landing/home/home.component.ts
+++ src/app/components/landing/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from "../about/about.component";
+import { WorkExperienceComponent } from "../work-experience/work-experience.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutComponent],
+  imports: [AboutComponent, WorkExperienceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
